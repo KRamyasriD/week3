398cf6392b0c710bfb97f63f24591c9cdef1120f
KRamyasriD <116748195+KRamyasriD@users.noreply.github.com>
2023-01-17 16:34:46 -0500

    Add files via upload

---
+++ Day4Assignment/ServiceNowAssingment.java
@@ -0,0 +1,58 @@
+package week3.Day4Assignment;
+
+import java.time.Duration;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class ServiceNowAssingment {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+//		https://dev110326.service-now.com/
+//			Username: admin
+//			Password: India@123
+//
+//			1. switchTo() frame using ID /Name
+//			2. Enter username
+//			3. Enter password
+//			4. Click Login button
+//			
+		
+		 ChromeDriver driver=new ChromeDriver();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		 //driver.get("https://dev110326.service-now.com/");
+		 
+		 driver.get("https://dev107885.service-now.com/");
+		 
+		 driver.manage().window().maximize();
+		 
+		 Thread.sleep(3000);
+		 
+		// driver.switchTo().frame("gsft_main");
+		 
+		 driver.findElement(By.id("user_name")).sendKeys("admin");
+		 
+		 driver.findElement(By.id("user_password")).sendKeys("1XJd3=%xqeAZ");
+		 
+		 driver.findElement(By.id("sysverb_login")).click();
+		 
+		 String text = driver.getTitle();
+		 
+		 System.out.println(text);
+		  
+		if (text.contains("Unified Navigation App")) {
+				
+				System.out.println("Loggedin Sucecssfully");
+			
+			}
+			else
+				
+				System.out.println("Failed login");
+			
+		}
+		 
+	}
+	
